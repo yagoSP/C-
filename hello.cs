92b5717bf57b83a4f9ee65641cfbe48603a69c64
Yago <54825415+yagoSP@users.noreply.github.com>
2024-08-08 13:34:19 -0300

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,16 @@
+using System;
+namespace HelloWorld
+{ 
+  class Hello
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("Aperte qualquer tecla para sair!");
+      Console.ReadKey();
+    }
+  }
+}
+
+//Hoje vai ser a PRIMEIRA aula do módulo de Desenvolimento (SENAC RIBEIRÃO PRETO TI30) :)
+// Escrito por YagoSP 08/08/2024 13:45 PM
