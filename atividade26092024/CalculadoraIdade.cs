085b83d0c23acad229e9074b8b20d7afef4f84ba
Yago <54825415+yagoSP@users.noreply.github.com>
2024-09-26 21:47:38 -0300

    Create CalculadoraIdade.cs

---
+++ atividade26092024/CalculadoraIdade.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i_idade = 0;
+            Console.Write("Digite a idade: ");
+            i_idade = Convert.ToInt16(Console.ReadLine());
+
+            if(i_idade >= 0 && i_idade <= 12)
+            {
+                Console.Write("Criança!");
+            }
+            else
+            {
+                if(i_idade >= 13 && i_idade <= 17)
+                {
+                    Console.Write("Adolescente!");
+                }
+                else
+                {
+                    if(i_idade >= 18 && i_idade <= 64)
+                    {
+                        Console.Write("Adulto!");
+                    }
+                    else
+                    {
+                       if(i_idade > 65)
+                        {
+                            Console.Write("Idoso!");
+                        }
+                        else
+                        {
+                            Console.Write("Idade Invalida!");
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//Criado por YagoSP 26/09/2024 21:45
