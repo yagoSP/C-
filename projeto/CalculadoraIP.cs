d0597440dea1229de5ecb37fbdf80723e18cf466
Yago <54825415+yagoSP@users.noreply.github.com>
2024-09-28 19:48:45 -0300

    Update CalculadoraIP.cs

--- projeto/CalculadoraIP.cs
+++ projeto/CalculadoraIP.cs
@@ -18,20 +18,57 @@ namespace ConsoleApp3
             int i_teroc = 0;
             int i_quaoc = 0;
 
+	    int i_counter = 0;
             int i_cidr = 0;
 	    string subnetmask = "";
 
             Console.Write("Digite o ip: ");
             s_ip = Console.ReadLine();
-            string[] s_ipqb = s_ip.Split('.');
-            Console.Write("Digite o valor do CIDR: ");
-            i_cidr = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine();
-       
+  	    
+	    if(s_ip == "")
+	    {
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.Write("DIGITE UM IP!\n");
+		Console.ResetColor();
+		return;
+	    }	
+	
+	    string[] s_ipqb = s_ip.Split('.');
+                 
 	    i_prioc = Convert.ToInt16(s_ipqb[0]);
             i_segoc = Convert.ToInt16(s_ipqb[1]);
             i_teroc = Convert.ToInt16(s_ipqb[2]);
             i_quaoc = Convert.ToInt16(s_ipqb[3]);
+	  
+
+	    for(i_counter = 0; i_counter <= 3; i_counter++)
+	    {
+	    	if(Convert.ToInt16(s_ipqb[i_counter]) > 255 || Convert.ToInt16(s_ipqb[i_counter]) < 0)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("IPV4 INVALIDO!\n");
+			Console.ResetColor();
+			return;
+		}
+	    }
+	  
+	    Console.Write("Digite o valor do CIDR: ");
+	    i_cidr = Convert.ToInt16(Console.ReadLine());
+	
+
+	    if(i_cidr > 32 || i_cidr < 0)
+	    {
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.Write("CIDR INVALIDO!\n");
+		Console.ResetColor();
+		return;
+	    }
+	    Console.WriteLine();
+
+	    Console.ForegroundColor = ConsoleColor.Blue;
+	    Console.WriteLine(s_ip);
+
+	    
 
 
 	    //Descobrir classe
@@ -39,18 +76,36 @@ namespace ConsoleApp3
             if (i_prioc >= 1 && i_prioc <= 127)
             {
                 Console.Write("CLASSE DO IP ---> [A]");
+		if(i_cidr != 8)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write("\nPOSSIVEL SUB REDE DETECTADA!");
+			Console.ForegroundColor = ConsoleColor.Green;
+		}
             }
             else
             {
                 if(i_prioc >= 128 && i_prioc <= 191)
                 {
-                    Console.Write("CLASSE DO IP ---> [B]");
+			Console.Write("CLASSE DO IP ---> [B]");
+			if(i_cidr != 16)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+                    		Console.Write("\nPOSSIVEL SUB REDE DETECTADA!");
+				Console.ForegroundColor = ConsoleColor.Green;
+			}
                 }
                 else
                 {
                     if(i_prioc >= 192 && i_prioc <= 255)
                     {
-                        Console.Write("CLASSE DO IP ---> [C]");
+			Console.Write("CLASSE DO IP ---> [C]");
+			if(i_cidr != 24)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+                	        Console.Write("\nPOSSIVEL SUB REDE DETECTADA!");
+				Console.ForegroundColor = ConsoleColor.Green;
+			}
                     }
                     else
                     {
@@ -58,6 +113,11 @@ namespace ConsoleApp3
                     }
                 }
             }
+	    
+	    
+
+
+
 
 	    //Quantidade de hosts
             Console.Write("\nQUANTIDADE DE HOSTS = {0}\n", Math.Pow(2, 32-i_cidr)-2);
@@ -72,7 +132,9 @@ namespace ConsoleApp3
 
 	    Console.ReadLine();
         }
-	
+
+
+
 
 	//função para mascara de subrede
 	public static string cidrtomask(int i_cidr)
