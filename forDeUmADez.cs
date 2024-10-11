9850e21d3c1dd255667bda7fd7ac9942a2b3589a
Yago <54825415+yagoSP@users.noreply.github.com>
2024-10-11 20:01:15 -0300

    Create forDeUmADez.cs

---
+++ forDeUmADez.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
+//Criado por yagosp 11/10/2024 19:59
