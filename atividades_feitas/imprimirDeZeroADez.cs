731fc7af5f116f0c2f22521bbfe723bad1c76b86
Yago <54825415+yagoSP@users.noreply.github.com>
2024-10-24 20:25:09 -0300

    Create imprimirDeZeroADez.cs

---
+++ atividades_feitas/imprimirDeZeroADez.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//CRIADO POR YAGOSP 24/10/2024
